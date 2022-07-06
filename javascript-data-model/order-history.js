c8669f46e2bcf9d70502caa55cf3e999ae488f5f
Francis Manalang <francismanalang7@gmail.com>
2022-07-06 20:06:30 +0000

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,112 @@
+var orderHistory = [
+  [
+    {
+      orderPlaced: 'August 4, 2020',
+      total: '$34.00',
+      shipTo: 'JS Masher',
+      order: [
+        {
+          orderNumber: '114-3941689-8772232'
+        },
+        'Order Details',
+        'Invoice'
+      ]
+    },
+    {
+      deliveredDate: 'Aug 8, 2020',
+      deliveryUpdate: '',
+      product: [
+        {
+          title: 'JavaScript for impatient programmers',
+          additionalInfo: 'Rauschmayer, Dr. Axel',
+          returnWindow: 'Return window closed on Sep 7, 2020',
+          price: '$31.55'
+        }
+      ]
+    }
+  ],
+  [
+    {
+      orderPlaced: 'July 19, 2020',
+      total: '$44.53',
+      shipTo: 'JS Masher',
+      order: [
+        {
+          orderNumber: '113-9984268-1280257'
+        },
+        'Order Details',
+        'Invoice'
+      ]
+    },
+    {
+      deliveredDate: 'Jul 20,2020',
+      deliveryUpdate: 'Your package was delivered. It was handed directly to a resident.',
+      product: [
+        {
+          title: 'The Timeless Way of Building',
+          additionalInfo: 'Alexander, Christopher',
+          returnWindow: 'Return window closed on Aug 19, 2020',
+          price: '$41.33'
+        }
+      ]
+    }
+  ],
+  [
+    {
+      orderPlaced: 'July 4, 2020',
+      total: '$17.22',
+      shipTo: 'JS Masher',
+      order: [
+        {
+          orderNumber: '114-2875557-9059409'
+        },
+        'Order Details',
+        'Invoice'
+      ]
+    },
+    {
+      deliveredDate: 'Jul 7, 2020',
+      deliveryUpdate: 'Your package was delivered. It was handed directly to a resident.',
+      product: [
+        {
+          title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+          additionalInfo: '',
+          returnWindow: 'Return window closed on Aug 5, 2020',
+          price: '$15.98'
+        }
+      ]
+    }
+  ],
+  [
+    {
+      orderPlaced: 'July 3, 2020',
+      total: '$138.98',
+      shipTo: 'JS Masher',
+      order: [
+        {
+          orderNumber: '113-2883177-2648248'
+        },
+        'Order Details',
+        'Invoice'
+      ]
+    },
+    {
+      deliveredDate: 'Jul 5, 2020',
+      deliveryUpdate: '',
+      product: [
+        {
+          title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+          additionalInfo: '',
+          returnWindow: 'Return window closed on Aug 4, 2020',
+          price: '$94.95'
+        },
+        {
+          title: 'The Art of SQL',
+          additionalInfo: 'Faroult, Stephane',
+          returnWindow: 'Return window closed on Aug 4, 2020',
+          price: '$33.99'
+        }
+      ]
+    }
+  ]
+];
