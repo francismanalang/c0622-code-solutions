19ef34de78924cb0218fbd97f0bb657870095d58
Francis Manalang <francismanalang7@gmail.com>
2022-07-12 21:55:44 +0000

    Completed javascript-light-bulb exercise

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,16 +1,17 @@
-var lightToggle = 0;
+var lightToggle = true;
 
 var lightBulb = document.querySelector('.light-on');
 var lightBackground = document.querySelector('.background-color');
 
 function handleLightToggle(event) {
-  lightToggle++;
-  if (lightToggle % 2 === 0) {
+  if (lightToggle) {
     lightBulb.className = 'light-on';
     lightBackground.className = 'container height-width background-color';
-  } else if (lightToggle % 2 === 1) {
+    lightToggle = false;
+  } else if (!lightToggle) {
     lightBulb.className = 'light-off';
     lightBackground.className = 'container height-width background-color1';
+    lightToggle = true;
   }
 }
 
