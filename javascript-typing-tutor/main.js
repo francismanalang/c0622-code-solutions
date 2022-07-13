674938e31dc0385c87bff45dcfd6144bbf821c19
Francis Manalang <francismanalang7@gmail.com>
2022-07-13 02:35:32 +0000

    Completed javascript-typing-tutor challenge

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,18 +1,16 @@
 var letters = document.querySelectorAll('span');
 
-for (var i = 0; i < letters.length; i++) {
-  var letter = letters[i];
-  letter.addEventListener('keydown', function typingTutor(event) {
-    if (letter[0] === 'g') {
-      letter[0].className = 'color';
-      letter[1].className = 'line';
-    } else {
-      letter[0].className = 'line wrong-color';
-    }
-    if (letter[1] === 'r') {
-      letter[1].className = 'color';
-    } else {
-      letter[1].className = 'line wrong-color';
-    }
-  });
+var counter = 0;
+
+function typingTutor(event) {
+  if (letters[counter].textContent === event.key) {
+    letters[counter].className = 'color';
+    letters[counter + 1].className = 'line';
+    counter++;
+
+  } else if (letters[counter].textContent !== event.key) {
+    letters[counter].className = 'wrong-line wrong-color';
+  }
 }
+
+document.addEventListener('keydown', typingTutor);
