4e92d01bbc5f4a5c524c289cf08b14ab72462ccd
Francis Manalang <francismanalang7@gmail.com>
2022-07-14 19:49:47 +0000

    dom-event-delegation optimized

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -3,10 +3,11 @@ var $taskList = document.querySelector('.task-list');
 function handleTaskList(event) {
   console.log('event.target:', event.target);
   console.log('event.target.tagName:', event.target.tagName);
+
   if (event.target.tagName === 'BUTTON') {
-    event.target.closest('.task-list-item');
-    console.log('closest task-list-item:', event.target.closest('.task-list-item'));
-    event.target.closest('.task-list-item').remove();
+    var $taskListItem = event.target.closest('.task-list-item');
+    console.log('closest task-list-item:', $taskListItem);
+    $taskListItem.remove();
   }
 }
 
