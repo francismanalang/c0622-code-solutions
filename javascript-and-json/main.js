5bd3e2e829e962078bd1823b8012ab604a18599a
Francis Manalang <francismanalang7@gmail.com>
2022-07-15 18:47:34 +0000

    Completed javascript-and-json exercise

---
+++ javascript-and-json/main.js
@@ -0,0 +1,31 @@
+var books = [
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson'
+  },
+  {
+    isbn: '978-0692232699',
+    title: 'Build APIs You Won\'t Hate',
+    author: 'Phil Sturgeon & Laura Bohill'
+  },
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation',
+    author: 'Lindsay Bassett'
+  }
+];
+
+console.log('books:', books);
+console.log('typeof books:', typeof books);
+
+var booksStringify = JSON.stringify(books);
+console.log('JSON.stringify(books):', booksStringify);
+console.log('typeof booksStringify:', typeof booksStringify);
+
+var student = '{"id":12345, "name": "Francis Manalang"}';
+console.log('student:', student);
+console.log('typeof student:', typeof student);
+
+var studentParse = JSON.parse(student);
+console.log('JSON.parse(student):', studentParse);
