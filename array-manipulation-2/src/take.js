1c2700f3a93b9c4b2329b32ab1f3e0ece33ef971
Francis Manalang <francismanalang7@gmail.com>
2022-07-15 19:39:39 +0000

    array-manipulation-2 progress

--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,1 +1,8 @@
 /* exported take */
+// define the function
+// use the slice method to return the array at count - 1
+// return the new slice variable from the function
+
+function take(array, count) {
+  return array.slice(count);
+}
