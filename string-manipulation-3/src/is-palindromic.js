add92aacdb26effdc8286aaa222909af0f8f9731
Francis Manalang <francismanalang7@gmail.com>
2022-07-17 23:18:55 +0000

    string manipulation 3 progress

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,26 +1,25 @@
 /* exported isPalindromic */
 // define the function
 // assign an empty string into the variable newString
-// write a for loop to iterate through the string backwards
-// add the values of string[i] into newString
-// if the newString is equal to string, return true, if not, return false
+// write a for loop that iterates through the string
+// within the for loop, write an if statement that pushes every letter into the variable newString if its not a space
+// write another for loop that iterates backwards through the newString variable
+// add the letters of the newString variable into the variable finalString
+// write an if statement that checks if newString and finalString are equal
+// if they're equal, return true, if not, return false
 
 function isPalindromic(string) {
   var newString = '';
-  for (var k = 0; k < string.length; k++) {
-    if (string[k] === ' ') {
-      var stringArray = string.split(' ');
-      newString += stringArray[1];
-      newString += stringArray[0];
-    }
-    if (newString === string) {
-      return newString;
+  var finalString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      newString += string[i];
     }
   }
-  for (var i = string.length - 1; i >= 0; i--) {
-    newString += string[i];
+  for (var k = newString.length - 1; k >= 0; k--) {
+    finalString += newString[k];
   }
-  if (newString === string) {
+  if (newString === finalString) {
     return true;
   } else {
     return false;
