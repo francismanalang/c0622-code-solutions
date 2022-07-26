526dc2bfaf5bacca3d0d2ce8ad75ec0765255796
Francis Manalang <francismanalang7@gmail.com>
2022-07-26 23:42:23 +0000

    Completed JavaScript Bank exercise

---
+++ javascript-bank/transaction.js
@@ -0,0 +1,14 @@
+/* exported Transaction */
+
+function Transaction(type, amount) {
+  this.type = type;
+  this.amount = amount;
+}
+
+Transaction.prototype.type = function () {
+  return this.type;
+};
+
+Transaction.prototype.amount = function () {
+  return this.amount;
+};
