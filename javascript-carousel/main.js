89539b3ace799c762851fc8268a606053cde581a
Francis Manalang <francismanalang7@gmail.com>
2022-07-29 04:11:56 +0000

    Completed javascript-carousel exercise

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,97 +1,69 @@
-var $imageButton1 = document.querySelector('.image1');
-var $imageButton2 = document.querySelector('.image2');
-var $imageButton3 = document.querySelector('.image3');
-var $imageButton4 = document.querySelector('.image4');
-var $imageButton5 = document.querySelector('.image5');
+var $buttons = document.querySelector('.icon-wrapper');
 var $arrowLeft = document.querySelector('.arrow-left');
 var $arrowRight = document.querySelector('.arrow-right');
 var $image = document.querySelector('.image');
+var imageIndex = 0;
+var $allButtons = document.querySelectorAll('.image-button');
+var imagesArray = [
+  'images/025.png', 'images/001.png', 'images/004.png', 'images/007.png', 'images/039.png'
+];
 
-function imageOneHandle(event) {
-  $image.setAttribute('src', 'images/025.png');
-  $imageButton1.className = 'image1 fa-solid fa-face-grin-stars fa-xl';
-  $imageButton2.className = 'image2 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton3.className = 'image3 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton4.className = 'image4 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton5.className = 'image5 fa-regular fa-face-grin-stars fa-xl';
+var intervalId = setInterval(arrowRightHandle, 3000);
+
+function parentHandle(event) {
+  for (var i = 0; i < $allButtons.length; i++) {
+    if ($allButtons[i] === event.target) {
+      $image.setAttribute('src', imagesArray[i]);
+      $allButtons[i].className = 'image-button fa-solid fa-face-grin-stars fa-xl';
+      imageIndex = i;
+    } else {
+      $allButtons[i].className = 'image-button fa-regular fa-face-grin-stars fa-xl';
+    }
+  }
+  clearInterval(intervalId);
+  intervalId = setInterval(arrowRightHandle, 3000);
 }
 
-$imageButton1.addEventListener('click', imageOneHandle);
+$buttons.addEventListener('click', parentHandle);
 
-function imageTwoHandle(event) {
-  $image.setAttribute('src', 'images/001.png');
-  $imageButton1.className = 'image1 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton2.className = 'image2 fa-solid fa-face-grin-stars fa-xl';
-  $imageButton3.className = 'image3 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton4.className = 'image4 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton5.className = 'image5 fa-regular fa-face-grin-stars fa-xl';
+function arrowButtonMatch() {
+  for (var k = 0; k < $allButtons.length; k++) {
+    if (k === imageIndex) {
+      $allButtons[k].className = 'image-button fa-solid fa-face-grin-stars fa-xl';
+    } else {
+      $allButtons[k].className = 'image-button fa-regular fa-face-grin-stars fa-xl';
+    }
+  }
 }
-
-$imageButton2.addEventListener('click', imageTwoHandle);
-
-function imageThreeHandle(event) {
-  $image.setAttribute('src', 'images/004.png');
-  $imageButton1.className = 'image1 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton2.className = 'image2 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton3.className = 'image3 fa-solid fa-face-grin-stars fa-xl';
-  $imageButton4.className = 'image4 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton5.className = 'image5 fa-regular fa-face-grin-stars fa-xl';
-}
-
-$imageButton3.addEventListener('click', imageThreeHandle);
-
-function imageFourHandle(event) {
-  $image.setAttribute('src', 'images/007.png');
-  $imageButton1.className = 'image1 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton2.className = 'image2 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton3.className = 'image3 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton4.className = 'image4 fa-solid fa-face-grin-stars fa-xl';
-  $imageButton5.className = 'image5 fa-regular fa-face-grin-stars fa-xl';
-}
-
-$imageButton4.addEventListener('click', imageFourHandle);
-
-function imageFiveHandle(event) {
-  $image.setAttribute('src', 'images/039.png');
-  $imageButton1.className = 'image1 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton2.className = 'image2 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton3.className = 'image3 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton4.className = 'image4 fa-regular fa-face-grin-stars fa-xl';
-  $imageButton5.className = 'image5 fa-solid fa-face-grin-stars fa-xl';
-}
-
-$imageButton5.addEventListener('click', imageFiveHandle);
 
 function arrowRightHandle(event) {
-  if ($image.getAttribute('src') === 'images/025.png') {
-    imageTwoHandle();
-  } else if ($image.getAttribute('src') === 'images/001.png') {
-    imageThreeHandle();
-  } else if ($image.getAttribute('src') === 'images/004.png') {
-    imageFourHandle();
-  } else if ($image.getAttribute('src') === 'images/007.png') {
-    imageFiveHandle();
-  } else if ($image.getAttribute('src') === 'images/039.png') {
-    imageOneHandle();
+  $image.setAttribute('src', imagesArray[imageIndex + 1]);
+  imageIndex++;
+  arrowButtonMatch();
+
+  if (imageIndex > 4) {
+    $image.setAttribute('src', imagesArray[0]);
+    imageIndex = 0;
+    arrowButtonMatch();
   }
+  clearInterval(intervalId);
+  intervalId = setInterval(arrowRightHandle, 3000);
 }
 
 $arrowRight.addEventListener('click', arrowRightHandle);
 
 function arrowLeftHandle(event) {
-  if ($image.getAttribute('src') === 'images/025.png') {
-    imageFiveHandle();
-  } else if ($image.getAttribute('src') === 'images/039.png') {
-    imageFourHandle();
-  } else if ($image.getAttribute('src') === 'images/007.png') {
-    imageThreeHandle();
-  } else if ($image.getAttribute('src') === 'images/004.png') {
-    imageTwoHandle();
-  } else if ($image.getAttribute('src') === 'images/001.png') {
-    imageOneHandle();
+  $image.setAttribute('src', imagesArray[imageIndex - 1]);
+  imageIndex--;
+  arrowButtonMatch();
+
+  if (imageIndex < 0) {
+    $image.setAttribute('src', imagesArray[4]);
+    imageIndex = 4;
+    arrowButtonMatch();
   }
+  clearInterval(intervalId);
+  intervalId = setInterval(arrowRightHandle, 3000);
 }
 
 $arrowLeft.addEventListener('click', arrowLeftHandle);
-
-setInterval(arrowRightHandle, 3000);
