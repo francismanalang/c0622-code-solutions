f2a13f79ff0bb899684dbd8da1d327cf10015828
Francis Manalang <francismanalang7@gmail.com>
2022-08-08 01:27:59 +0000

    String manipulation 3 algorithms progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,16 @@
 /* exported reverseWords */
+
+function reverseWords(string) {
+  var splitWords = string.split(' ');
+  var word = '';
+  var reverseWords = [];
+
+  for (var i = 0; i < splitWords.length; i++) {
+    for (var k = splitWords[i].length - 1; k >= 0; k--) {
+      word += splitWords[i][k];
+    }
+    reverseWords.push(word);
+    word = '';
+  }
+  return reverseWords.join(' ');
+}
