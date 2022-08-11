9db1b4814aec71bc30ef6f3ee1f6f15c40af76b4
Francis Manalang <francismanalang7@gmail.com>
2022-08-11 18:22:00 +0000

    Completed algorithms with updated pseudo code

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,4 +1,12 @@
 /* exported reverseWords */
+// use split to turn the string into an array of words
+// assign an empty string to the variable word
+// assign an empty array to the variable reverseWords
+// write a for loop that iterates through the array of words
+// within the code block, write another for loop that iterates through each string in the array starting from the end
+// add each letter to the variable word
+// each time the word is reversed, it is pushed into the reverseWords array and the variable word is equal to an empty string
+// once the for loop has completed, use the join method on the reverseWords array and return it from the function
 
 function reverseWords(string) {
   var splitWords = string.split(' ');
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,4 +1,24 @@
 /* exported titleCase */
+// use split to turn the string into an array of words
+// assign an empty array in the variable finalTitle
+// Write a for loop that takes each word and capitalizes the first letter of each word and pushes it to the finalTitle array
+// Write another for loop that checks for specific words such as Api and JavaScript and change them accordingly
+// use the splice method to replace them in the array
+// write an if statement to check for short words that should be lower cased
+// use the splice method to replace them in the finalTitle array
+// write anthor if statement to check for the word 'The'
+// lowercase the word if it is not the first letter of the title
+// write another if statement that checks for the word 'the'
+// if the previous word includes colon, then capitalize the first letter
+// write an if statement that checks if a word includes a dash symbol
+// if it does, split the word at the dash and capitalize the first letter of the second word
+// join the word with a dash and use the splice method to insert the new word in the finalTitle array
+// write an if statement that checks if a word includes a colon
+// if it does, split the word at the colon
+// write an if statement that checks if the first word in the array is equal to 'Javascript'
+// if it is true, replace it with 'JavaScript'
+// join the array with a colon and use the splice method to replace the word in the array
+// finally, use the join mehtod to join the finalTitle array with a space and return it from the function
 
 function titleCase(string) {
   var title = string.toLowerCase().split(' ');
