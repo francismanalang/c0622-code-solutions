9798201d35623eedbfaa6cc8a42eb2ff006f2e86
Francis Manalang <francismanalang7@gmail.com>
2022-08-18 00:32:22 +0000

    Completed node-fs-writefile exercise

---
+++ node-fs-readfile/cat.js
@@ -0,0 +1,13 @@
+// const fs = require('fs');
+// let index = 0;
+
+// const file = process.argv.slice(2);
+// fs.readFile(file, 'utf8', (err, data) => {
+//   var string = '';
+//   if (err) throw err;
+//   if (index < file.length) {
+//     string += file[index];
+//     index++;
+//   }
+//   console.log(string);
+// });
