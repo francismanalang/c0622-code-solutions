0ac95457b02055d6591144ced6a54de04c589bf5
Francis Manalang <francismanalang7@gmail.com>
2022-08-19 21:29:37 +0000

    Completed express-get-json exercise

--- express-get-json/index.js
+++ express-get-json/index.js
@@ -22,7 +22,9 @@ const grades = {
   }
 };
 
-app.listen(3000);
+app.listen(3000, () => {
+  // console.log('Listening on port 3000!');
+});
 
 app.get('/api/grades', (req, res) => {
   var gradesArray = [];
