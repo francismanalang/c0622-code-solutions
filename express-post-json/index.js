5a58c9e332ba66954bed4d7080746cc1bb4b083b
Francis Manalang <francismanalang7@gmail.com>
2022-08-20 00:48:35 +0000

    Completed express-post-json exercise

---
+++ express-post-json/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+let nextId = 1;
+const grades = {};
+
+app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+  for (const id in grades) {
+    gradesArray.push(grades[id]);
+  }
+  res.json(gradesArray);
+});
+
+app.post('/api/grades', (req, res) => {
+  const newGrade = req.body;
+  newGrade.id = nextId;
+  grades[nextId] = newGrade;
+  nextId++;
+  res.send(newGrade);
+  res.status(201);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
