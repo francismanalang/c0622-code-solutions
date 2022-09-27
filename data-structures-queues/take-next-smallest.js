cf56ae4427bb6531b69d562d9329455e0b23bca3
Francis Manalang <francismanalang7@gmail.com>
2022-09-27 13:43:34 +0000

    Data Structures Queues progress

---
+++ data-structures-queues/take-next-smallest.js
@@ -0,0 +1,9 @@
+/* exported takeNextSmallest */
+
+function takeNextSmallest(queue) {
+  // const firstItem = queue.dequeue();
+  // if (queue.peek() === undefined) {
+  //   return firstItem;
+  // }
+  // const secondItem = queue.peek();
+}
